b4ece7963046593a3f998fd0f008458955ac6a26
Wright, Eron <Eron.Wright@emc.com>
2017-03-24 17:33:50 -0700

    STRM-131 calculate daily summary

--- pravega-flink-examples/src/main/dist/bin/run-example.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env bash
-
-flink run -c com.emc.pravega.examples.flink.TurbineHeatProcessor lib/pravega-flink-examples-0.0-PRERELEASE-all.jar
--- pravega-flink-examples/src/main/scala/com/emc/pravega/examples/flink/TurbineHeatProcessor.scala
+++ pravega-flink-examples/src/main/scala/com/emc/pravega/examples/flink/TurbineHeatProcessor.scala
@@ -7,12 +7,17 @@ package com.emc.pravega.examples.flink
 
 import java.net.URI
 
+import scala.math._
 import com.emc.pravega.connectors.flink.FlinkPravegaReader
 import com.emc.pravega.examples.flink.Streams._
 import com.emc.pravega.examples.flink.serialization.PravegaDeserializationSchema
 import com.emc.pravega.stream.impl.{ControllerImpl, JavaSerializer}
 import org.apache.flink.api.java.utils.ParameterTool
+import org.apache.flink.streaming.api.TimeCharacteristic
+import org.apache.flink.streaming.api.functions.timestamps.BoundedOutOfOrdernessTimestampExtractor
+import org.apache.flink.streaming.api.windowing.time.Time
 import org.apache.flink.streaming.api.scala._
+import org.apache.flink.streaming.api.windowing.assigners.TumblingEventTimeWindows
 
 import scala.collection.JavaConversions._
 import scala.concurrent.Await
@@ -26,6 +31,21 @@ import scala.concurrent.duration._
   */
 object TurbineHeatProcessor {
 
+  case class SensorEvent(timestamp: Long, sensorId: Int, location: String, temp: Float)
+
+  case class SensorAggregate(sensorId: Int, location: String, tempRange: (Float,Float))
+
+  object SensorAggregate {
+    val nothing = null.asInstanceOf[SensorAggregate]
+    def fold(acc: SensorAggregate, evt: SensorEvent): SensorAggregate = {
+      acc match {
+        case null =>
+          SensorAggregate(evt.sensorId, evt.location, (evt.temp, evt.temp))
+        case _ =>
+          SensorAggregate(evt.sensorId, evt.location, (min(evt.temp, acc.tempRange._1), max(acc.tempRange._2, evt.temp)))
+      }
+    }
+  }
 
   def main(args: Array[String]) {
     val params = Parameters(ParameterTool.fromArgs(args))
@@ -46,18 +66,38 @@ object TurbineHeatProcessor {
 
     // set up the streaming execution environment
     val env = StreamExecutionEnvironment.getExecutionEnvironment
+    env.setStreamTimeCharacteristic(TimeCharacteristic.EventTime)
 
     // construct a dataflow graph
-    val events: DataStream[String] = env.addSource(new FlinkPravegaReader(
+
+    // 1. read and decode the sensor events from a Pravega stream
+    val events: DataStream[SensorEvent] = env.addSource(new FlinkPravegaReader(
       params.controllerUri,
       params.scope,
       Set(params.stream),
       params.startTime,
-      new PravegaDeserializationSchema(classOf[String], new JavaSerializer[String]())))
+      new PravegaDeserializationSchema(classOf[String], new JavaSerializer[String]()))).map { line =>
+      val l = line.trim.split(", ")
+      SensorEvent(l(0).toLong, l(1).toInt, l(2), l(3).toFloat)
+    }.name("events")
 
-    events.print()
+    // 2. extract timestamp information to support 'event-time' processing
+    val timestamped = events.assignTimestampsAndWatermarks(
+      new BoundedOutOfOrdernessTimestampExtractor[SensorEvent](Time.seconds(10)) {
+        override def extractTimestamp(element: SensorEvent): Long = element.timestamp
+      })
 
-    env.execute("Turbine Heat Processor")
+    // 3. summarize the temperature data for each sensor
+    val summaries = timestamped
+      .keyBy("sensorId")
+      .window(TumblingEventTimeWindows.of(Time.days(1),Time.hours(+8)))
+      .fold(SensorAggregate.nothing)(SensorAggregate.fold)
+      .name("summaries")
+
+    // 4. print to stdout.  Refer to the TaskManager's 'Stdout' view in the Flink UI.
+    summaries.print().name("stdout")
+
+    env.execute(s"Turbine Heat Processor (${params.scope}, ${params.stream})")
   }
 
   private def ensureStream(params: Parameters) = {
