1fcac8d42806a6818141f2d7c0ab247c2fce67f7
Flavio Junqueira (fpj) <flavio.junqueira@emc.com>
2017-04-09 10:35:18 +0100

    Fixing also in Constants.java.

--- standalone-examples/src/main/java/com/emc/pravega/example/consolerw/Constants.java
+++ standalone-examples/src/main/java/com/emc/pravega/example/consolerw/Constants.java
@@ -6,6 +6,6 @@ package com.emc.pravega.example.consolerw;
  */
 public class Constants {
     protected static final String DEFAULT_SCOPE = "examples";
-    protected static final String DEFAULT_STREAM_NAME = "some_stream";
+    protected static final String DEFAULT_STREAM_NAME = "someStream";
     protected static final String DEFAULT_CONTROLLER_URI = "tcp://127.0.0.1:9090";
 }
