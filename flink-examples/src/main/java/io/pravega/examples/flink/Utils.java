3ebbec44ac4c1cdf0a971c8a952c3ad1f9b1e076
Eron Wright <eronwright@gmail.com>
2018-05-17 10:45:13 -0700

    [Issue-76] update samples to new builder API (#79)
    - rework samples to use the new builder API of the Flink connector
    - use `PravegaConfig` to establish a Pravega context (connection info, default scope)
    - set stream scale policy to 1 for event time-based apps
    - updated help text of anomaly app
    
    Signed-off-by: Eron Wright <eronwright@gmail.com>

---
+++ flink-examples/src/main/java/io/pravega/examples/flink/Utils.java
@@ -0,0 +1,51 @@
+/*
+ * Copyright (c) 2018 Dell Inc., or its subsidiaries. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ */
+package io.pravega.examples.flink;
+
+import io.pravega.client.admin.StreamManager;
+import io.pravega.client.stream.Stream;
+import io.pravega.client.stream.StreamConfiguration;
+import io.pravega.connectors.flink.PravegaConfig;
+
+public class Utils {
+
+    /**
+     * Creates a Pravega stream with a default configuration.
+     *
+     * @param pravegaConfig the Pravega configuration.
+     * @param streamName the stream name (qualified or unqualified).
+     */
+    public static Stream createStream(PravegaConfig pravegaConfig, String streamName) {
+        return createStream(pravegaConfig, streamName, StreamConfiguration.builder().build());
+    }
+
+    /**
+     * Creates a Pravega stream with a given configuration.
+     *
+     * @param pravegaConfig the Pravega configuration.
+     * @param streamName the stream name (qualified or unqualified).
+     * @param streamConfig the stream configuration (scaling policy, retention policy).
+     */
+    public static Stream createStream(PravegaConfig pravegaConfig, String streamName, StreamConfiguration streamConfig) {
+        // resolve the qualified name of the stream
+        Stream stream = pravegaConfig.resolve(streamName);
+
+        try(StreamManager streamManager = StreamManager.create(pravegaConfig.getClientConfig())) {
+            // create the requested scope (if necessary)
+            streamManager.createScope(stream.getScope());
+
+            // create the requested stream based on the given stream configuration
+            streamManager.createStream(stream.getScope(), stream.getStreamName(), streamConfig);
+        }
+
+        return stream;
+    }
+}
