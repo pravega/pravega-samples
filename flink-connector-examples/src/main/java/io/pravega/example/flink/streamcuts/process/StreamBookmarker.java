b708e94d7fd24520e625fcaea81bd5f313ddfd4c
Raúl Gracia <raul.gracia@emc.com>
2018-10-27 21:32:40 +0200

    Issue 160: Use new StreamCuts API call to get the end of the slice.
    Signed-off-by: Raúl Gracia <raul.gracia@emc.com>

--- flink-connector-examples/src/main/java/io/pravega/example/flink/streamcuts/process/StreamBookmarker.java
+++ flink-connector-examples/src/main/java/io/pravega/example/flink/streamcuts/process/StreamBookmarker.java
@@ -12,7 +12,6 @@ package io.pravega.example.flink.streamcuts.process;
 
 import io.pravega.client.admin.ReaderGroupManager;
 import io.pravega.client.admin.StreamManager;
-import io.pravega.client.segment.impl.Segment;
 import io.pravega.client.stream.ReaderGroup;
 import io.pravega.client.stream.Stream;
 import io.pravega.client.stream.StreamCut;
@@ -27,7 +26,8 @@ import io.pravega.example.flink.streamcuts.SensorStreamSlice;
 import io.pravega.example.flink.streamcuts.serialization.Tuple2DeserializationSchema;
 import java.io.IOException;
 import java.net.URI;
-import java.util.Map;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.ScheduledThreadPoolExecutor;
 import org.apache.flink.api.common.state.ValueState;
 import org.apache.flink.api.common.state.ValueStateDescriptor;
 import org.apache.flink.api.common.typeinfo.TypeHint;
@@ -111,16 +111,18 @@ public class StreamBookmarker {
  * the state of the Flink reader. The main task of this class is to output SensorStreamSlice objects that represent events
  * created by DataProducer whose value is < 0.
  */
-class Bookmarker extends ProcessFunction<Tuple2<Integer, Double>, SensorStreamSlice>{
+class Bookmarker extends ProcessFunction<Tuple2<Integer, Double>, SensorStreamSlice> {
 
     private static final Logger LOG = LoggerFactory.getLogger(Bookmarker.class);
     private final URI pravegaControllerURI;
     private ReaderGroup readerGroup;
+    private transient ScheduledExecutorService executor;
 
     private transient ValueState<SensorStreamSlice> pendingBookmark;
 
     public Bookmarker(URI pravegaControllerURI) {
         this.pravegaControllerURI = pravegaControllerURI;
+        this.executor = new ScheduledThreadPoolExecutor(1);
     }
 
     @Override
@@ -147,32 +149,23 @@ class Bookmarker extends ProcessFunction<Tuple2<Integer, Double>, SensorStreamSl
         if (value.f1 < 0 && pendingBookmark.value() == null) {
             // Instantiate a SensorStreamSlice object for this sensor.
             SensorStreamSlice sensorStreamSlice = new SensorStreamSlice(value.f0);
-
             // Set the current ReaderGroup StreamCut as the beginning of the slice of events of interest.
-            StreamCut startStreamCut = getReaderGroup().getStreamCuts().get(Stream.of(Constants.DEFAULT_SCOPE, Constants.PRODUCER_STREAM));
+            StreamCut startStreamCut = getReaderGroup().getStreamCuts()
+                                                       .get(Stream.of(Constants.DEFAULT_SCOPE, Constants.PRODUCER_STREAM));
             sensorStreamSlice.setStart(startStreamCut);
             pendingBookmark.update(sensorStreamSlice);
             LOG.warn("Start bookmarking a stream slice at: {} for sensor {}.", startStreamCut, value.f0);
         } else if (value.f1 >= 0 && pendingBookmark.value() != null) {
             SensorStreamSlice sensorStreamSlice = pendingBookmark.value();
-            StreamCut currentStreamCut = getReaderGroup().getStreamCuts().get(Stream.of(Constants.DEFAULT_SCOPE, Constants.PRODUCER_STREAM));
-
-            // If this is the first event > 0, then we need to keep the current StreamCut. Note that this is needed to
-            // check for the next StreamCut strictly beyond the current reader position, as the current StreamCut is
-            // likely to represent past reader positions that may not contain all events < 0 for this sensor.
-            if (sensorStreamSlice.getEnd() == null) {
-                LOG.warn("Initialize sensorStreamSlice end value to look for the next updated StreamCut: {} for sensor {}.", currentStreamCut, value.f0);
-                sensorStreamSlice.setEnd(currentStreamCut);
-            } else if (checkUpdatedStreamCutPositions(sensorStreamSlice.getEnd(), currentStreamCut)) {
-                // Only when all the positions in the previous StreamCut are updated in currentStreamCut, we can ensure
-                // that the slice contains at least all the evens of interest.
-                sensorStreamSlice.setEnd(currentStreamCut);
-                out.collect(sensorStreamSlice);
-                LOG.warn("Found next end StreamCut for sensor {}: {}. The slice should contain all events < 0 for a specific sensor sine wave.", value.f0, currentStreamCut);
-                sensorStreamSlice = null;
-            }
-
-            pendingBookmark.update(sensorStreamSlice);
+            // We found the first event > 0, so we want a StreamCut from this point onward to complete the slice.
+            StreamCut currentStreamCut = getReaderGroup().generateStreamCuts(executor)
+                                                         .join()
+                                                         .get(Stream.of(Constants.DEFAULT_SCOPE, Constants.PRODUCER_STREAM));
+            sensorStreamSlice.setEnd(currentStreamCut);
+            out.collect(sensorStreamSlice);
+            LOG.warn("Found next end StreamCut for sensor {}: {}. The slice should contain all events < 0 for a specific sensor sine wave.",
+                    value.f0, currentStreamCut);
+            pendingBookmark.update(null);
         }
     }
 
@@ -190,27 +183,6 @@ class Bookmarker extends ProcessFunction<Tuple2<Integer, Double>, SensorStreamSl
         }
 
         return readerGroup;
-    }
-
-    /**
-     * This method compares the positions of two StreamCuts. Concretely, this method verifies that all the positions
-     * from the first input StreamCut are higher compared to the second input argument. This ensures that all the
-     * reading positions of interest for the first StreamCut have been updated in the second one.
-     *
-     * @param streamCut StreamCut at a given point in time.
-     * @param toCompare StreamCut to check if all its reading positions are higher compared to the first input argument.
-     * @return Whether all the common positions between two StreamCuts are higher for the second one.
-     */
-    private boolean checkUpdatedStreamCutPositions(StreamCut streamCut, StreamCut toCompare) {
-        Map<Segment, Long> streamCutPositions = streamCut.asImpl().getPositions();
-        Map<Segment, Long> toComparePositions = toCompare.asImpl().getPositions();
-        for (Segment s: streamCutPositions.keySet()) {
-            if (toComparePositions.containsKey(s) && toComparePositions.get(s) <= streamCutPositions.get(s)) {
-                return false;
-            }
-        }
-
-        return true;
     }
 }
 
