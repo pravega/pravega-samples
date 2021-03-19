2ed765db69295ad9c2b50888bc0ce0e8780b36a7
Claudio Fahey <claudio.fahey@dell.com>
2021-03-19 17:29:57 +0000

    Fix samples version number and improve feedback in runAtLastOnceApp.sh
    Signed-off-by: Claudio Fahey <claudio.fahey@dell.com>

--- scripts/runAtLeastOnceApp.sh
+++ scripts/runAtLeastOnceApp.sh
@@ -2,13 +2,19 @@
 set -ex
 ROOT_DIR=$(dirname $0)/..
 cd ${ROOT_DIR}
-./gradlew pravega-client-examples:build
+./gradlew pravega-client-examples:distTar
 cd pravega-client-examples/build/distributions
-tar -xf pravega-client-examples-0.7.0.tar
-cd pravega-client-examples-0.7.0
+tar -xf pravega-client-examples-0.10.0-SNAPSHOT.tar
+cd pravega-client-examples-0.10.0-SNAPSHOT
 
 NUM_INSTANCES=${1:-1}
 
 for i in $(seq -w 01 $NUM_INSTANCES); do
-  INSTANCE_ID=$i bin/atLeastOnceApp >& /tmp/atLeastOnceApp-$i.log &
+  LOG_FILE=/tmp/atLeastOnceApp-$i.log
+  echo Logging to ${LOG_FILE}
+  INSTANCE_ID=$i bin/atLeastOnceApp >& ${LOG_FILE} &
 done
+
+echo Instances have been started as background jobs.
+
+tail -f /tmp/atLeastOnceApp-*.log
