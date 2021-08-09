a4b2d24b9091a49db6fa8fa151f8fc59daa5452c
thekingofcity <3353040+thekingofcity@users.noreply.github.com>
2021-08-09 10:18:56 +0800

    seperate port change to another issue
    Signed-off-by: thekingofcity <3353040+thekingofcity@users.noreply.github.com>

--- flink-connector-examples/src/main/java/io/pravega/example/flink/wordcount/Constants.java
+++ flink-connector-examples/src/main/java/io/pravega/example/flink/wordcount/Constants.java
@@ -18,7 +18,7 @@ public class Constants {
     protected static final String HOST_PARAM = "host";
     protected static final String DEFAULT_HOST = "127.0.0.1";
     protected static final String PORT_PARAM = "port";
-    protected static final String DEFAULT_PORT = "9998";
+    protected static final String DEFAULT_PORT = "9999";
     protected static final String STREAM_PARAM = "stream";
     protected static final String DEFAULT_SCOPE = "examples";
     protected static final String DEFAULT_STREAM = "wordcount";
