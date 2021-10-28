9a2cc19451eac739a8433b2103a6dcce0f5612e8
thekingofcity <3353040+thekingofcity@users.noreply.github.com>
2021-10-28 11:44:16 +0800

    Use jdbc for metadata table
    Signed-off-by: thekingofcity <3353040+thekingofcity@users.noreply.github.com>

--- scenarios/change-data-capture/flink/init.sql
+++ scenarios/change-data-capture/flink/init.sql
@@ -14,12 +14,13 @@ CREATE TABLE metadata (
   id VARCHAR(10) NOT NULL PRIMARY KEY,
   sector STRING NOT NULL
 ) WITH (
-  'connector' = 'pravega',
-  'controller-uri' = 'tcp://pravega:9090',
-  'scope' = 'stock',
-  'scan.execution.type' = 'streaming',
-  'scan.streams' = 'dbserver1.stock.metadata',
-  'format' = 'debezium-json'
+  'connector' = 'jdbc',
+  'url' = 'jdbc:mysql://mysql:3306/stock',
+  'table-name' = 'metadata',
+  'username' = 'root',
+  'password' = 'dbz',
+  'lookup.cache.max-rows' = '50',
+  'lookup.cache.ttl' = '60min'
 );
 
 CREATE TABLE `index` (
